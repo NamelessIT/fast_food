d134303032d3c573105b04b54bd07f788d004f16
dai-huu <152070668+dai-huu@users.noreply.github.com>
2024-10-21 17:00:36 +0700

    Update AccountResource.php (change icon to heroicon-s-wrench)

--- app/Filament/Resources/AccountResource.php
+++ app/Filament/Resources/AccountResource.php
@@ -23,7 +23,7 @@ class AccountResource extends Resource
 {
     protected static ?string $model = Account::class;
 
-    protected static ?string $navigationIcon = 'heroicon-s-users';
+    protected static ?string $navigationIcon = 'heroicon-s-wrench';
 
     public static function form(Form $form): Form
     {
