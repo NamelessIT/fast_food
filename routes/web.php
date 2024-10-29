77c3ddb3df94020c2a947b68ce3b1d63128752f0
EpikPotato <duccute3@gmail.com>
2024-10-29 10:11:45 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Account\AccountController;
 use App\Models\account;
 use App\Models\Product;
 use App\Http\Controllers\Home\HomeController;
+use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 // use App\Database\DbConnection;
 
@@ -18,9 +19,10 @@ Route::group(['prefix' => '/'], function () {
 });
 
 Route::group(['prefix' => '/product'], function () {
-    Route::get('/', function () {
+    /*Route::get('/', function () {
         return view('products.welcome');
-    });
+    });*/
+    Route::get('/', [ProductController::class, 'index'])->name('product.index');
 
     Route::get ('/detail-product/{id}', function () {
         return view('products.detail-product');
