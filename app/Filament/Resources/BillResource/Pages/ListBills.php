31147aca3b63152f6f69c843abbf86c6bfe716b5
daihuu <hodai04vn@gmail.com>
2024-11-22 08:08:24 +0700

    thêm tab lọc ở phía trên Bills

--- app/Filament/Resources/BillResource/Pages/ListBills.php
+++ app/Filament/Resources/BillResource/Pages/ListBills.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\BillResource\Pages;
 use App\Filament\Resources\BillResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListBills extends ListRecords
 {
@@ -16,4 +18,19 @@ class ListBills extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getTabs(): array
+{
+    return [
+        'all' => Tab::make('All'),
+        'pending' => Tab::make('Đang chờ')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 1)),
+        'confirmed' => Tab::make('Đã xác nhận')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 2)),
+        'delivered' => Tab::make('Đã giao')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 3)),
+        'cancel' => Tab::make('Đã hủy')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 0)),
+    ];
+}
 }
