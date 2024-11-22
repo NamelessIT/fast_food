c59d49e6fdfdf66b358e23e5c3cc09e49cc48308
daihuu <hodai04vn@gmail.com>
2024-11-22 16:22:40 +0700

    có thể dùng username hoặc email để đăng nhập vào admin

---
+++ app/Filament/Auth/CustomLogin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Auth;
+
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Auth\Login;
+use Illuminate\Validation\ValidationException;
+
+class CustomLogin extends Login
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getLoginlFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        //$this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getLoginlFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label(__('Username / Email'))
+            ->rules(['required'])
+            ->validationMessages([
+                'required' => 'Bạn chưa nhập username hoặc email'
+            ])
+            ->autocomplete()
+            ->markAsRequired()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return TextInput::make('password')
+            ->label(__('Password'))
+            ->rules(['required'])
+            ->password()
+            ->revealable()
+            ->markAsRequired()
+            ->validationMessages([
+                'required' => 'Bạn chưa nhập mật khẩu',
+            ])
+            ->extraInputAttributes(['tabindex' => 2]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        return [
+            $login_type => $data['login'],
+            'password' => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __('Username/Email hoặc mật khẩu không chính xác. Vui lòng thử lại.'),
+        ]);
+    }
+}
